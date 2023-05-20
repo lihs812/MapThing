0811790cfddef56f12200d30dafd6df85ff93fe0
werhiuqhoru <jjh030726@kaka.com>
2023-05-20 12:21:20 +0900

    commit

--- app/src/main/java/com/example/mapthing/MainActivity.java
+++ app/src/main/java/com/example/mapthing/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.mapthing;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
+    /*@PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String content;
+
+    private  boolean completed;
+     */
     //Test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
