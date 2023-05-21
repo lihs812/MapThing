3c0b07665d9c862213787645efe4ba35de86a687
werhiuqhoru <jjh030726@kaka.com>
2023-05-21 15:17:59 +0900

    commit

---
+++ app/src/main/java/com/example/mapthing/CustomCaptureAdapter.java
@@ -0,0 +1,91 @@
+package com.example.mapthing;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CustomCaptureAdapter extends BaseAdapter{
+
+
+    private static final String TAG = "CustomCustomCaptureClass";
+
+    private ArrayList<CustomCaptureClass> captureItems = new ArrayList<>();
+    private ViewHolder mViewHolder;
+    private Context mContext;
+
+    public CustomCaptureAdapter(Context context) {
+        this.mContext = context;
+    }
+
+    public class ViewHolder {
+        private ImageView ivCapture;
+        private CheckBox chkBox;
+        private TextView titleTextView;
+        private TextView categoryTextView;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.layout_custom_capture_item, parent, false);
+
+            mViewHolder = new ViewHolder();
+
+            mViewHolder.ivCapture = convertView.findViewById(R.id.iv_photo_item);
+
+            mViewHolder.titleTextView = convertView.findViewById(R.id.tv_item_title);
+            mViewHolder.categoryTextView = convertView.findViewById(R.id.tv_item_category);
+
+            convertView.setTag(mViewHolder);
+
+        } else {
+            mViewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        CustomCaptureClass captureItem = captureItems.get(position);
+
+        //Data 세팅
+        mViewHolder.ivCapture.setImageBitmap(captureItem.getCapture());
+        mViewHolder.chkBox.setChecked(false);
+        mViewHolder.titleTextView.setText(captureItem.getCustomTitle());
+        mViewHolder.categoryTextView.setText(captureItem.getCategory());
+
+        return convertView;
+    }
+
+    @Override
+    public int getCount() {
+        return captureItems.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return captureItems.get(position);
+    }
+
+    public void addItem(String strTitle, String strCategory) {
+        CustomCaptureClass item = new CustomCaptureClass();
+
+        item.setCustomTitle(strTitle);
+        item.setCategory(strCategory);
+        item.setStatus(false);
+
+        captureItems.add(item);
+    }
+
+}
+
--- app/src/main/java/com/example/mapthing/MainActivity.java
+++ app/src/main/java/com/example/mapthing/MainActivity.java
@@ -1,81 +1,33 @@
 package com.example.mapthing;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String customTitle; // 이름
-    private String realPath; // 실제 경로
-    private String category; // 분류
-    private Bitmap capture; // 이미지 파일
-    private Boolean status; // 상태
-    private String tag; // tag
-
-    public String getCustomTitle() {
-        return customTitle;
-    }
-
-    public void setCustomTitle(String customTitle) {
-        this.customTitle = customTitle;
-    }
-
-    public String getRealPath() {
-        return realPath;
-    }
-
-    public void setRealPath(String realPath) {
-        this.realPath = realPath;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public Bitmap getCapture() {
-        return capture;
-    }
-
-    public void setCapture(Bitmap capture) {
-        this.capture = capture;
-    }
-
-    public Boolean getStatus() {
-        return status;
-    }
-
-    public void setStatus(Boolean status) {
-        this.status = status;
-    }
-
-    public String getTag() {
-        return tag;
-    }
-
-    public void setTag(String tag) {
-        this.tag = tag;
-    }
-
-    @Override
-    public String toString() {
-        return "CustomCaptureClass [customTitle: " + customTitle
-                + ", category: " + category
-                + ", realPath: " + realPath
-                + ", capture: " + capture
-                + ", status: " + status
-                + ", tag: " + tag + "]";
-    }
-
+    private ListView listView;
     // Test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listViewCapture); //listview 선언
+
+        CustomCaptureAdapter customCaptureAdapter = new CustomCaptureAdapter(MainActivity.this); //CustomAdapter
+        listView.setAdapter(customCaptureAdapter); //ListView에 Adapter 연결
+
+        customCaptureAdapter.addItem("물건 1","001"); //Item 추가
+        customCaptureAdapter.addItem("물건 2","002");
+        customCaptureAdapter.addItem("물건 3","003");
+        customCaptureAdapter.addItem("물건 4","004");
+        customCaptureAdapter.addItem("물건 5","005");
+        customCaptureAdapter.addItem("물건 6","006");
+        customCaptureAdapter.addItem("물건 7","007");
+
+        customCaptureAdapter.notifyDataSetChanged(); //Adapter 변경시 적용
     }
+
 }
