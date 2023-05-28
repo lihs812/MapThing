6a56df92aa022c216843195fa818f2fbf98c5872
werhiuqhoru <jjh030726@kaka.com>
2023-05-29 00:06:39 +0900

    UI 수정중

--- app/src/main/java/com/example/mapthing/IN_List_View.java
+++ app/src/main/java/com/example/mapthing/IN_List_View.java
@@ -1,7 +1,6 @@
 package com.example.mapthing;
 
 
-
 //이 클래스는 리스트 안의 화면
 public class IN_List_View {
 
--- app/src/main/java/com/example/mapthing/MainActivity.java
+++ app/src/main/java/com/example/mapthing/MainActivity.java
@@ -3,12 +3,58 @@ package com.example.mapthing;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRv_mapthings;
+    private FloatingActionButton mBtn_write;
+    private ArrayList<Arritem> mGetAlist;
+    private GETDB mGETDB;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setInit();
     }
+
+    private void setInit() {
+        mGETDB = new GETDB(this);
+        mRv_mapthings = findViewById(R.id.rv_list);
+        mBtn_write = findViewById(R.id.floatingActionButton4);
+        mGetAlist = new ArrayList<>();
+
+
+        /*
+        mBtn_write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //팝업창
+                Dialog dialog = new Dialog(MainActivity.this, android.R.style.Theme_Material_Light_Dialog);
+                dialog.setContentView(R.layout.popup_edit);
+                EditText plain_text1 = dialog.findViewById(R.id.plain_text1);
+                Button btn_ok = dialog.findViewById(R.id.btn_ok);
+
+
+                btn_ok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String currentTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+
+                        mGETDB.InsertDATA(plain_text1.getText(),toString(),.getText().toString);
+                    }
+                });
+            }
+        });
+        */
+    }
+
 }
