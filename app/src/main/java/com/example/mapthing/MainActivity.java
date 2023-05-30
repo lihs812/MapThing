ccc4b3756f2d7c12f65ea3234511e4b52f62fbcf
werhiuqhoru <jjh030726@kaka.com>
2023-05-30 22:36:36 +0900

    05.30 수정 기능은 고치지 못했지만 검색 기능을 추가함(작동이 되는지 확인 못해서 주석처리함)

--- app/src/main/java/com/example/mapthing/MainActivity.java
+++ app/src/main/java/com/example/mapthing/MainActivity.java
@@ -2,13 +2,13 @@ package com.example.mapthing;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
 import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
@@ -17,6 +17,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mRv_mapthings;
@@ -25,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
     private DBHelper dbHelper;
     private Context mContext; // mContext 변수 추가
     private CustomAdapter mAdapter;
+    private List<String> list;          // 데이터를 넣은 리스트변수
+    private ListView listView;          // 검색을 보여줄 리스트변수
+    private EditText editSearch;        // 검색어를 입력할 Input 창
+    private ArrayList<String> arraylist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,6 +90,17 @@ public class MainActivity extends AppCompatActivity {
                 dialog.show();
             }
         });
+        // 검색 버튼 클릭 리스너 설정
+        /*
+        ImageView searchButton = findViewById(R.id.serch_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchText = mSerch_text_var.getText().toString();
+                search(searchText);
+            }
+        });
+         */
     }
 
     private void loadRecentDB() {
@@ -96,4 +112,25 @@ public class MainActivity extends AppCompatActivity {
             mRv_mapthings.setAdapter(mAdapter);
         }
     }
+    // 검색을 수행하는 메소드
+    /*
+    public void search(String searchText) {
+        // 문자 입력시마다 리스트를 지우고 새로 뿌려준다.
+        mAlist.clear();
+
+        // 문자 입력이 없을때는 모든 데이터를 보여준다.
+        if (searchText.length() == 0) {
+            mAlist.addAll(dbHelper.getAlist());
+        }
+        // 문자 입력을 할때..
+        else {
+            // DB에서 검색된 데이터를 가져온다.
+            mAlist.addAll(dbHelper.searchAlist(searchText));
+        }
+
+        // 어댑터에 변경된 데이터를 알린다.
+        mAdapter.notifyDataSetChanged();
+    }
+
+     */
 }
