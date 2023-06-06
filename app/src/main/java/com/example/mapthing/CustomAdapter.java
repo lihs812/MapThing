e41c18401e71e4a023a59a2055ce08324a238894
werhiuqhoru <jjh030726@kaka.com>
2023-06-06 15:52:28 +0900

    버그 내용(팝업창에서 새로 추가한 제품의 태그명과 물건의 경로 값이 바뀌는 현상이 있음, 앱을 재실행 하면 정상으로 돌아옴)

--- app/src/main/java/com/example/mapthing/CustomAdapter.java
+++ app/src/main/java/com/example/mapthing/CustomAdapter.java
@@ -31,11 +31,13 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
         this.mContext = mContext;
         this.mGETDB = mGETDB;
     }
+
     public void updateData(ArrayList<Arritem> data) {
         mAlist.clear();
         mAlist.addAll(data);
         notifyDataSetChanged();
     }
+
     private String formatDateString(String fullDate) {
         try {
             SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -48,12 +50,11 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
         }
     }
 
-
     @NonNull
     @Override
     public CustomAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View holder = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
-        return new ViewHolder(holder);
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
+        return new ViewHolder(itemView);
     }
 
     @Override
@@ -61,11 +62,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
         Arritem item = mAlist.get(position);
         holder.custom_title.setText(item.getTitle());
         holder.tag_name.setText(item.getTag());
-
-        String fullDate = item.getWriteDate();
-        String formattedDate = formatDateString(fullDate);
-        holder.write_date.setText(formattedDate);
-
+        holder.write_date.setText(formatDateString(item.getWriteDate()));
     }
 
     @Override
@@ -79,7 +76,6 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
         private TextView tag_name;
         private TextView write_date;
 
-
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
@@ -88,10 +84,9 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
             write_date = itemView.findViewById(R.id.write_date);
 
             itemView.setOnClickListener(new View.OnClickListener() {
-                // 수정된 CustomAdapter 클래스의 ViewHolder 내의 onClick 메서드
                 @Override
                 public void onClick(View v) {
-                    int curpos = getAdapterPosition();  // 현재 리스트 클릭한 아이템 위치
+                    int curpos = getAdapterPosition();
                     Arritem arritem = mAlist.get(curpos);
 
                     String[] strChoiceItems = {"수정하기", "삭제하기"};
@@ -101,8 +96,6 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
                         @Override
                         public void onClick(DialogInterface dialogInterface, int position) {
                             if (position == 0) {
-                                // 수정하기
-                                // 팝업창
                                 Dialog dialog = new Dialog(mContext, android.R.style.Theme_Material_Light_Dialog);
                                 dialog.setContentView(R.layout.popup_edit);
                                 EditText plain_text1 = dialog.findViewById(R.id.plain_text1);
@@ -110,15 +103,13 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
                                 EditText path_name = dialog.findViewById(R.id.path_name);
                                 Button btn_ok = dialog.findViewById(R.id.btn_ok);
 
-                                // 기존 데이터로 기본 값을 설정
                                 plain_text1.setText(arritem.getTitle());
                                 tag_name.setText(arritem.getTag());
-                                path_name.setText(arritem.getPath()); // 이 부분을 추가하여 기본 값을 설정합니다.
+                                path_name.setText(arritem.getPath());
 
                                 btn_ok.setOnClickListener(new View.OnClickListener() {
                                     @Override
                                     public void onClick(View v) {
-                                        // DB에 데이터 수정
                                         String currentTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
                                         mGETDB.Update(
                                                 plain_text1.getText().toString(),
@@ -129,11 +120,10 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
                                                 ""
                                         );
 
-                                        // UI에 아이템 수정
                                         arritem.setTitle(plain_text1.getText().toString());
                                         arritem.setTag(tag_name.getText().toString());
                                         arritem.setWriteDate(currentTime);
-                                        arritem.setPath(path_name.getText().toString()); // 이 부분을 추가하여 경로 값을 업데이트합니다.
+                                        arritem.setPath(path_name.getText().toString());
                                         notifyDataSetChanged();
 
                                         dialog.dismiss();
@@ -143,11 +133,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
                                 dialog.show();
 
                             } else if (position == 1) {
-                                // 삭제하기
-                                // DB에서 데이터 삭제
                                 mGETDB.Delete(arritem.getTitle());
-
-                                // UI에서 아이템 삭제
                                 mAlist.remove(curpos);
                                 notifyItemRemoved(curpos);
                                 Toast.makeText(mContext, "물건 정보가 삭제되었습니다.", Toast.LENGTH_SHORT).show();
@@ -156,14 +142,12 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
                     });
                     builder.show();
                 }
-
             });
         }
     }
 
-    //현재 어댑터에 새로운 게시글 아이템을 전달받아 추가 함
-    public void addItem(Arritem _item)  {
+    public void addItem(Arritem _item) {
         mAlist.add(0, _item);
-        notifyItemInserted(0); //새로고침
+        notifyItemInserted(0);
     }
 }
